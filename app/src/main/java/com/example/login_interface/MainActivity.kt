37dec2132ac20a510cc32fab8168443d5f95c0c2
Tafari Vilanculos <tafarivilanculos@gmail.com>
2024-01-24 20:43:49 +0200

    Verification Part

--- app/src/main/java/com/example/login_interface/MainActivity.kt
+++ app/src/main/java/com/example/login_interface/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.login_interface
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.login_interface.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.btnEntrar.setOnClickListener {
+            var id = binding.txtID.text.toString().trim()
+            var senha= binding.txtSenha.text.toString().trim()
+            if(id.isEmpty()|| senha.isEmpty())
+            {
+                Toast.makeText(this, "Campos vazios", Toast.LENGTH_LONG).show()
+            }else if(id.equals("123")&& senha.equals("Tafari")){
+                Toast.makeText(this, "Seja Bem Vindo :)", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this,"id ou senha incorrecta",Toast.LENGTH_LONG).show()
+            }
+        }
+
+
+
     }
 }
