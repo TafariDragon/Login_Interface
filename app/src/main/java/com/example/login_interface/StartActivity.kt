8cf45b752c2b950bc5277f41524702c966ce6508
Tafari Vilanculos <tafarivilanculos@gmail.com>
2024-02-03 23:04:11 +0200

    Criação de uma spash screen

---
+++ app/src/main/java/com/example/login_interface/StartActivity.kt
@@ -0,0 +1,19 @@
+package com.example.login_interface
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },3000)
+    }
+}
